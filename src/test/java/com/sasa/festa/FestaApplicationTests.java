f1027e51f53a26708f8e6ea45dbd7c1c018bc8be
unknown <souzalaurindo49@gmail.comgit config --global user.email souzalaurindo49@gmail.com>
2025-08-21 22:10:31 -0300

    Projeto Criado

---
+++ src/test/java/com/sasa/festa/FestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasa.festa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
